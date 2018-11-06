7075322597add362d33e908b1e905712f895e945
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-06 16:46:57 +0000

    add play against computer version. Add pictures

--- controller.rb
+++ controller.rb
@@ -8,7 +8,26 @@ get '/rockpaperscissors/:hand1/:hand2' do
   hand2 = params[:hand2]
 
   game = RockPaperScissors.new(hand1, hand2)
-
+  @player1 = hand1
+  @player2 = hand2
   @rps_result = game.play()
   erb(:result)
+end
+
+get '/playcomputer/:hand1' do
+  hand1 = params[:hand1]
+  hand2 = ['rock', 'paper', 'scissors'].sample
+  game = RockPaperScissors.new(hand1, hand2)
+  @human_choice = hand1
+  @computer_choice = hand2
+  @comp_result = game.play
+  erb(:comp_result)
+end
+
+get '/rules' do
+ erb(:rules)
+end
+
+get '/' do
+  erb(:home)
 end
